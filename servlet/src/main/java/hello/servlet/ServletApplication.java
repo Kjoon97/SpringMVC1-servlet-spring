f283a3ef26efebd860c091cc8eb6e35cec054b5d
JoonHyuk <jh83370@naver.com>
2022-01-26 10:41:15 +0900

    서블릿 등록, 서블릿 request, response 객체 사용

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //스프링이 현재 내 패키지(hello.servlet)의 하위 패키지들 다 뒤져서 서블릿을 찾아서 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="helloServlet", urlPatterns = "/hello") //name은 원하는 대로, 주소창에 '/hello'로 들어오면 이것이 실행되는 것.
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");  //http 쿼리 파라미터를 읽기.(/hello?username='kim')
+        System.out.println("username 은 " + username);
+
+        response.setContentType("text/plain");  //단순 문자형식  (이 정보는 http 헤더에 들어감)
+        response.setCharacterEncoding("utf-8");  //문자 인코딩   (이 정보는 http 헤더에 들어감)
+        response.getWriter().write("hello "+username);   //http 메세지 바디에 데이터를 넣기
+    }
+}
