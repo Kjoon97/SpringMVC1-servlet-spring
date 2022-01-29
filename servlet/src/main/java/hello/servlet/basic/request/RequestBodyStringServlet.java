f70050859306873e297c799d86b809452ee603f5
JoonHyuk <jh83370@naver.com>
2022-01-30 01:44:08 +0900

    HTTP 요청 데이터-API 메시지 바디(단순 텍스트)

---
+++ servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name="requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet{
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ServletInputStream inputStream = request.getInputStream();   //메세지 바디의 내용을 바이트 코드로 가져올 수 있음
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);//바이트 코드를 string으로 바꾸기, 스프링이 제공하는 클래스StreamUtils를 이용.
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
+}
