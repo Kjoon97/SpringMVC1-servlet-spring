6da7da7898a047553d51a84e8ddc3e045423faa9
joonhyuck <jh83370@naver.com>
2022-02-04 18:41:08 +0900

    뷰 리졸버

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan //스프링이 현재 내 패키지(hello.servlet)의 하위 패키지들 다 뒤져서 서블릿을 찾아서 자동 등록
 @SpringBootApplication
@@ -12,4 +15,8 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+//	@Bean
+//	ViewResolver internalResourceViewResolver(){
+//		return new InternalResourceViewResolver("/WEB-INF/views/",".jsp");
+//	}  -> 스프링부트가 이것을 application.properties 정보를 보고 자동으로 해줌.
 }
